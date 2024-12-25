848cae3c556e755c0460d30a7dfff4bf5973226a
Billy Kid Andersson <billy.andersson@student.htw-berlin.de>
2024-12-25 14:27:15 +0100

    fixed dropdown showing multiple instances of bewirtschafter, also changed display of parentid to not display memory locations and instead clear text names

--- src/main/java/com/arbeit/bachelor/controller/SBKManagementController.java
+++ src/main/java/com/arbeit/bachelor/controller/SBKManagementController.java
@@ -2,6 +2,7 @@ package com.arbeit.bachelor.controller;
 
 import com.arbeit.bachelor.model.Bewirtschafter;
 import com.arbeit.bachelor.model.SBK;
+import com.arbeit.bachelor.repository.BewirtschafterRepository;
 import com.arbeit.bachelor.repository.SBKRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,15 +18,19 @@ import java.util.List;
 public class SBKManagementController {
 
     private final SBKRepository sbkRepository;
+    private final BewirtschafterRepository bewirtschafterRepository;
 
-    public SBKManagementController(SBKRepository sbkRepository) {
+    public SBKManagementController(SBKRepository sbkRepository, BewirtschafterRepository bewirtschafterRepository) {
         this.sbkRepository = sbkRepository;
+        this.bewirtschafterRepository = bewirtschafterRepository;
     }
 
     @GetMapping
     public String viewSBKManagementPage(Model model) {
-        List<SBK> allSbks = sbkRepository.findAll();
+        List<SBK> allSbks = sbkRepository.findAll(); // Fetch all SBKs
+        List<Bewirtschafter> bewirtschafterList = bewirtschafterRepository.findAll();
         model.addAttribute("sbks", allSbks);
+        model.addAttribute("bewirtschafterList", bewirtschafterList);
 
         SBK defaultSbk = new SBK();
         defaultSbk.setBewirtschafter(new Bewirtschafter());
@@ -41,12 +46,16 @@ public class SBKManagementController {
             return "sbk_management";
         }
 
+        // Filter SBKs based on the selected Bewirtschafter's name
         List<SBK> filteredSbks = sbkRepository.findByBewirtschafter_Name(
                 selectedSbk.getBewirtschafter().getName()
         );
 
+        List<Bewirtschafter> bewirtschafterList = bewirtschafterRepository.findAll();
         model.addAttribute("sbks", filteredSbks);
+        model.addAttribute("bewirtschafterList", bewirtschafterList);
         model.addAttribute("selectedSbk", selectedSbk);
+
         return "sbk_management";
     }
 }
